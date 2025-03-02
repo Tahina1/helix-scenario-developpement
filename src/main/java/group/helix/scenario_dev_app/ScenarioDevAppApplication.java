2054cca977af7637beca4e46532b89b1e6898aad
Tahina <tahinarkt123@gmail.com>
2025-03-03 00:00:58 +0300

    init project

---
+++ src/main/java/group/helix/scenario_dev_app/ScenarioDevAppApplication.java
@@ -0,0 +1,13 @@
+package group.helix.scenario_dev_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioDevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenarioDevAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/helix/scenario_dev_app/ScenarioDevAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group.helix.scenario_dev_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenarioDevAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
