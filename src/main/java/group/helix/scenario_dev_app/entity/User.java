895063b4f19584810a376d4bf5a291cc7d876f89
Tahina <tahinarkt123@gmail.com>
2025-03-04 01:01:26 +0300

    clean css, login.html

--- src/main/java/group/helix/scenario_dev_app/entity/User.java
+++ src/main/java/group/helix/scenario_dev_app/entity/User.java
@@ -16,7 +16,15 @@ public class User {
     private String username;
     private String password;
     private String role;
-
+    
+    public User() {
+    }
+    
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
     // Getters et Setters
     public Long getId() {
         return id;
